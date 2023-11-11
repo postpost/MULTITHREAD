42fe665a893c2e8175ca4a2dccd86e90ca733a59
predttt <timofeevapolina88@gmail.com>
2023-11-11 16:33:32 -0300

    task_01 modified

--- HW_03/Task_01/Task_01/main.cpp
+++ HW_03/Task_01/Task_01/main.cpp
@@ -2,19 +2,25 @@
 #include <thread>
 #include <future>
 
-void sort(int* array, int size, std::promise<int*> prom) {
+int find_min(int* array, int smallest, int size, std::promise<int>& prom) {
+	//асинхронно запускается функция поиска минимума, который возвращается через std::future
+	for (int current = smallest + 1; current < size; ++current) {
+		if (array[current] < array[smallest]) {
+			smallest = current;
+		}
+	}
+	//prom.set_value(smallest);
+	return smallest;
+}
+
+void sort(int* array, int size, std::promise<int>& prom) {
 	int smallest = 0;
 	for (int start = 0; start < size; ++start) {
 		smallest = start;
-		for (int current = smallest + 1; current < size; ++current) {
-			if (array[current] < array[smallest]) {
-				smallest = current;
-			}
-		}
+		std::future ft = std::async(find_min, array, smallest,size, std::ref(prom));
+		smallest = ft.get();
 		std::swap(array[start], array[smallest]);
 	}
-	//prom.set_value(array[i]);
-	prom.set_value(array);
 }
 
 void print_array(int* array, const int size) {
@@ -31,10 +37,6 @@ void fill_arr(int* array, const int size) {
 	}
 }
 
-int sum(int a, int b) {
-	return a + b;
-}
-
 int main() {
 	int size = 10;
 	int* array = new int[size];
@@ -42,10 +44,9 @@ int main() {
 	print_array(array, size);
 
 	// async
-	std::promise<int*> prom;
-	std::future<int*> f1 = prom.get_future(); //connection
-	auto f2 = std::async(std::launch::async, sort, std::ref(array), size, std::move(prom));
-	array = f1.get();
+	std::promise<int> prom;
+	std::future<int> f_prom = prom.get_future(); //connection
+	sort(array, size, prom);
 	print_array(array, size);
 	delete[] array;
 	return 0;
